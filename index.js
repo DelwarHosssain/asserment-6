b3bb546b99708fab1ff99e8e0c4dd5b970c2e15d
DelwarHosssain <mddelwarhossainnobin12@gmail.com>
2023-09-02 21:17:23 +0600

    add-titel-oapi

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const hanglerCatagore = async () => {
     console.log(data.data);
 };
 const hendelAl = async (categoryId) => {
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const response = await fetch(` https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
     console.log(data.data);
 
@@ -23,16 +23,22 @@ const hendelAl = async (categoryId) => {
         const div=document.createElement('div')
         div.innerHTML =`
         
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card w-96 h-96 bg-base-100 shadow-xl">
                 <figure><img src="${all?.thumbnail}
                 " alt="Shoes" /></figure>
                 <div class="card-body ">
-                    <div class="flex justify-start">
-                    <img class="w-12" src="image/download.jpg" alt="">  
-                    <h2 class="card-title">Shoes!</h2>
-                    </div>
-                    <p>Awlad Hossain <span><i class="fa-solid fa-heart"></i></span></p>
-                    <div class="card-actions justify-center">
+                    <div class="flex  ">
+                    <div class="avatar">
+                            <div class="w-16 h-16 rounded-full ">
+                                <img src="${all?.authors[0].profile_picture}" />
+                            </div>
+                        </div>
+                        <div>
+                        <h2 class=" p-5  font-extrabold text-center text-lg ">${all.title}</h2>
+                        </div>
+                    </div >
+                    <p class="font-normal text-sm text-[rgba(23, 23, 23, 0.70);]">${all?.authors[0].profile_name} <span><i class="fa-solid fa-handshake "></i></span></p>
+                    <div class="card-actions justify-star font-normal text-sm text-[rgba(23, 23, 23, 0.70);]">
                     <h5>91K views</h5>
                     </div>
                 </div>
@@ -43,3 +49,20 @@ const hendelAl = async (categoryId) => {
 };
 
 hanglerCatagore();
+
+
+// posted_date
+// : 
+// "13885"
+// views
+// : 
+// "1.1K"
+// [[Prototype]]
+// : 
+// Object
+// thumbnail
+// : 
+// "https://i.ibb.co/NTncwqH/luahg-at-pain.jpg"
+// title
+// : 
+// "Laugh at My Pain"
